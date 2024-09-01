8b3b0654ed971ad6f25695a7a0605e99a5acb173
Ivan Sotelo <dev_ivansotelo@capitalcheckin.app>
2024-09-01 01:59:29 +0000

    fix: update

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Capital Check In'
+      }
+    }
+  },
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Capital-Check-In/capital-check-in-handbook',
   },
-  chat: {
-    link: 'https://discord.com',
+  docsRepositoryBase: 'https://github.com/Capital-Check-In/capital-check-in-handbook',
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Capital Check In',
+    text: (
+      <p className="mt-6 text-xs">
+          © {new Date().getFullYear()} Capital Check In.
+      </p>
+    ),
   },
+  toc: {
+    backToTop: true
+  }
 }
 
 export default config
